bc42f87b43680e3f9abc2d9988bf4809206814a9
toddtsic <toddtsic@gmail.com>
2023-09-16 15:30:26 -0700

    wired up hamburger icon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { StorageService } from './services/storage.service';
+import { environment } from 'src/environments/environment';
+import { Platform } from '@ionic/angular';
+import { Capacitor } from '@capacitor/core';
+import { App } from '@capacitor/app';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  appVersion: string;
+  isIOS = false;
+  tsicMenuLinks = [
+    {
+      title: 'Logout/Login',
+      url: '/login',
+      icon: 'log-in'
+    },
+    {
+      title: 'Switch to Different Team',
+      url: '/roles',
+      icon: 'cog'
+    }
+  ];
+
   constructor(
+    private platform: Platform,
     private storageService: StorageService, // must have to cause initialization of storageService
-  ) {}
+  ) {  
+  }
+
+  async ngOnInit(): Promise<void> {
+    if (environment.production) {
+      const ai = await App.getInfo();
+      this.appVersion = ai.version;
+    }
+
+    this.isIOS = this.platform.is('ios');
+
+    this.platform.ready()
+      .then(async (val) => {
+
+
+        const thisPlatform = Capacitor.getPlatform();
+        if (thisPlatform !== 'web') {
+          // this.initPushNotifications();
+        }
+      });
+  }
+
 }
--- src/app/pages/tabs/tabs.page.ts
+++ src/app/pages/tabs/tabs.page.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ITeamChat_User_RoleData } from 'src/app/models/roles-models';
+import { StorageService } from 'src/app/services/storage.service';
 
 @Component({
   selector: 'app-tabs',
   templateUrl: 'tabs.page.html',
   styleUrls: ['tabs.page.scss']
 })
-export class TabsPage {
+export class TabsPage implements OnInit {
+  userData: ITeamChat_User_RoleData;
 
-  constructor() {}
-
+  constructor(private storageService:StorageService) {}
+  
+  async ngOnInit() {
+    this.userData = await this.storageService.getUser_AuthSettings();
+  }
 }
