ccccded735d59d8591f75a6d8dbf3f0a62cf4940
toddtsic <toddtsic@gmail.com>
2023-09-17 09:39:02 -0700

    syncfusion license loaded in main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,13 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 import { defineCustomElements } from '@ionic/pwa-elements/loader';
+import { registerLicense } from '@syncfusion/ej2-base';
 
 // Call the element loader after the platform has been bootstrapped
 defineCustomElements(window);
 
+// Registering Syncfusion license key
+registerLicense('ORg4AjUWIQA/Gnt2VlhhQlJCfV5AQmBIYVp/TGpJfl96cVxMZVVBJAtUQF1hSn5ad0JiWHtYc3xdT2Rc')
 if (environment.production) {
   enableProdMode();
 }
