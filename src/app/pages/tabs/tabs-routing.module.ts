6fb8714fcc8661c0da7167c9458940c952a24124
toddtsic <toddtsic@gmail.com>
2023-09-16 17:50:16 -0700

    calendar ==> tab4

--- src/app/pages/tabs/tabs-routing.module.ts
+++ src/app/pages/tabs/tabs-routing.module.ts
@@ -22,6 +22,10 @@ const routes: Routes = [
         loadChildren: () => import('./tab3/tab3.module').then(m => m.Tab3PageModule)
       },
       {
+        path: 'tab4',
+        loadChildren: () => import('./tab4/tab4.module').then(m => m.Tab4PageModule)
+      },
+      {
         path: '',
         redirectTo: '/tabs/tab1',
         pathMatch: 'full'
@@ -32,6 +36,10 @@ const routes: Routes = [
     path: '',
     redirectTo: '/tabs/tab1',
     pathMatch: 'full'
+  },
+  {
+    path: 'tab4',
+    loadChildren: () => import('./tab4/tab4.module').then( m => m.Tab4PageModule)
   }
 ];
 
